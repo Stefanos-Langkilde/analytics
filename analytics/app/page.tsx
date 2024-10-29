d2cc2417525b704464496585696f78df28b0cb6b
Stefan Langkilde <eaa23stla@students.eaaa.dk>
2024-10-29 15:01:29 +0100

    Refactor mainPageStyle.module.scss to add header buttons for datepicker and radio dropdown

--- analytics/app/page.tsx
+++ analytics/app/page.tsx
@@ -3,8 +3,9 @@ import MyChart2 from "@/components/myChart2";
 import MyChart from "../components/myChart";
 import styles from "./mainPageStyle.module.scss";
 import MyChart3 from "@/components/myChart3";
-import Datepicker from "../components/datepicker";
 import LineChart from "@/components/lineChart";
+import Datepicker from "../components/datepicker";
+import RadioDropdown from "../components/radioDropdown";
 
 export default function Home() {
    // add a new chart to the dashboard by adding a new object to the charts array
@@ -21,7 +22,10 @@ export default function Home() {
          <div className={styles.header}>
             <h1>Welcome to your Analytics Dashboard</h1>
             <p>Here you can see all the data you need to make informed decisions.</p>
-            <Datepicker />
+            <div className={styles.headerButtons}>
+               <Datepicker />
+               <RadioDropdown />
+            </div>
          </div>
          <div className={styles.chartGrid}>
             {charts.map(chart => (
