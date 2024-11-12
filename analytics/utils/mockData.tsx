3eed0b9a13a7e5516c98aa3a61725d5e3cebdf13
Stefan Langkilde <eaa23stla@students.eaaa.dk>
2024-11-12 16:31:56 +0100

    Add mock data for analytics testing and development

---
+++ analytics/utils/mockData.tsx
@@ -0,0 +1,63 @@
+// Example of actual data
+export const mockData = [
+   {
+      date: "2024-11-01",
+      total: 1014.9,
+      orders: 4,
+   },
+   {
+      date: "2024-11-02",
+      total: 946.4,
+      orders: 3,
+   },
+   {
+      date: "2024-11-03",
+      total: 1308.7,
+      orders: 4,
+   },
+   {
+      date: "2024-11-04",
+      total: 8806.5,
+      orders: 30,
+   },
+   {
+      date: "2024-11-05",
+      total: 11361.8,
+      orders: 34,
+   },
+   {
+      date: "2024-11-06",
+      total: 17402.7,
+      orders: 59,
+   },
+   {
+      date: "2024-11-07",
+      total: 3159.2,
+      orders: 11,
+   },
+   {
+      date: "2024-11-08",
+      total: 1171.3,
+      orders: 7,
+   },
+   {
+      date: "2024-11-09",
+      total: 279.0,
+      orders: 1,
+   },
+   {
+      date: "2024-11-10",
+      total: 2272.4,
+      orders: 5,
+   },
+   {
+      date: "2024-11-11",
+      total: 4621.4,
+      orders: 8,
+   },
+   {
+      date: "2024-11-12",
+      total: 1290.8,
+      orders: 4,
+   },
+];
